aa89d69556dfb321826ae93d09febbc607d97a78
CrispyChillies <phamnguyenhailongct@gmail.com>
2025-08-22 17:08:40 +0700

    add the code for credit card processing

---
+++ dataset_download.py
@@ -0,0 +1,8 @@
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("mlg-ulb/creditcardfraud")
+
+print("Path to dataset files:", path)
+
+# kaggle competitions download -c nyc-taxi-trip-duration: Command to download the NYC City Taxi Trip
